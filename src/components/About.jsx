26ed341175a08cee1c449d1c5f8c84e8b99924c5
lukwiya23 <luk23bonnie8@gmail.com>
2023-03-29 00:46:55 +0300

    .

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Tilt from "react-tilt";
+import  Tilt  from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+
+
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
